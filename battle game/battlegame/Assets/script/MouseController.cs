623c25574bd83275e7564ff06fcf11f82988e2a6
lorenzo <lorenzo.boldrini15345@gmail.com>
2020-09-22 14:56:51 +0200

    set up scena

---
+++ battle game/battlegame/Assets/script/MouseController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MouseController : MonoBehaviour
+{
+    GameObject Soldier;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && Soldier == null)
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                if (hit.transform.gameObject.tag == "soldier")
+                {
+                    Soldier = hit.transform.gameObject;
+                    Soldier.GetComponent<MeshRenderer>().material.SetColor("_color", Color.green);
+                    print(Soldier);
+                }
+            }
+        }
+        else if (Input.GetMouseButtonDown(0) && Soldier != null)
+        {
+
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                if (hit.transform.gameObject.tag == "Plane")
+                {
+                    Soldier.GetComponent<NavMeshAgent>().destination = hit.point;
+                    Soldier.GetComponent<MeshRenderer>().material.SetColor("_color", Color.white);
+                    Soldier = null;
+                }
+            }
+        }
+    }
+}
