b22ebdb9f9562093be07db539466066cbfc77d72
lorenzo <lorenzo.boldrini15345@gmail.com>
2020-09-23 09:29:06 +0200

    camera_controller / aggiustamento mouse controller

--- battle game/battlegame/Assets/script/MouseController.cs
+++ battle game/battlegame/Assets/script/MouseController.cs
@@ -17,6 +17,38 @@ public class MouseController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began && Soldier == null)
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.touches[0].position);
+            if (Physics.Raycast(ray, out hit, ignoreRaycast))
+            {
+                if (hit.transform.gameObject.tag == "soldier")
+                {
+                    Soldier = hit.transform.gameObject;
+                    Soldier.GetComponent<MeshRenderer>().material.SetColor("_color", Color.green);
+                    print(Soldier);
+                }
+            }
+        }
+        else if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began && Soldier != null)
+        {
+
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.touches[0].position);
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                if (hit.transform.gameObject.tag == "Plane")
+                {
+                    Soldier.GetComponent<NavMeshAgent>().destination = hit.point;
+                    Soldier = null;
+                }
+            }
+        }
+
+
         if (Input.GetMouseButtonDown(0) && Soldier == null)
         {
             RaycastHit hit;
